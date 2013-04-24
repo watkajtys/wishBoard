997e6560ffa0ee86219b2b03d419d89d4d46b1ca
Matt Watkajtys <watkajtys@gmail.com>
2013-04-24 02:15:15 -0400

    Fixed bug with prependDriver
    Driver was defaulting to default value despite n being defined. Value of hidden form was never being updated.

--- app/assets/javascripts/prependDriver.js
+++ app/assets/javascripts/prependDriver.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-   var prepends=["I wish", "I want", "I miss"], n_max = 2, n = 0;
+   var prepends=["I wish", "I want", "I miss"], n_max = 2, n = Math.floor(Math.random()*(n_max+1));
+   
    $('.prompt').text(prepends[n])
+   $('#post_prepend').val(prepends[n]);
+
    $(".left, .right").click(function(){
       n += $(this).hasClass('left') ? 1 : -1;
       if (n > n_max) n = 0;
