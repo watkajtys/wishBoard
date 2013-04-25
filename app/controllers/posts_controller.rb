1047757cb4cdc57ae0f6c516b40c52c183e2c6dc
Matt Watkajtys <watkajtys@gmail.com>
2013-04-25 19:44:07 -0400

    Created errors partial to handle error reporting on app.
    Removed error reporting from -input partial and added it to _errors.
    Specified alert in post controller to send error messages over.
    Added ability to display specific error by parsing modified alert flash.
    Added container div around yield.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,9 @@ class PostsController < ApplicationController
    def create
       @post = Post.new params[:post]
       if @post.save
-         redirect_to [:root]
+         redirect_to [:root], notice: "Your wish has been added!"
+      else
+         redirect_to [:root], alert: @post.errors.full_messages
       end
    end
 
