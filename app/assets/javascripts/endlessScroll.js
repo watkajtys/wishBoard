968e2cc22f728250d7089943bbe17cba9d218af8
Matt Watkajtys <watkajtys@gmail.com>
2013-05-04 20:46:54 -0400

    Moved loading point for AJAX endless page up 100px.
    Page now loads more entries before user hits bottom of page.

--- app/assets/javascripts/endlessScroll.js
+++ app/assets/javascripts/endlessScroll.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
       var visibleHeight = document.documentElement.clientHeight;
       var currentScroll = $(document).scrollTop();
 
-      if (visibleHeight + currentScroll >= totalHeight && loading == false) {
+      if (visibleHeight + currentScroll >= totalHeight + 100px && loading == false) {
          page++;
          loading = true;
          console.log('bottom');
