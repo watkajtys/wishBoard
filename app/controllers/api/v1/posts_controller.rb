355adc793a47b4ede3f826bbfe2fd5240d0c7a78
Matt Watkajtys <watkajtys@gmail.com>
2013-05-10 17:11:21 -0400

    Added functional tests for the API controller.
    Had to change the post controller to make it work with the API.
    Error reporting now gone, will have to investigate this later.

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -7,12 +7,12 @@ class Api::V1::PostsController < ApplicationController
    end
 
    def create
-      @post = Post.new params[:post]
-      if @post.save
-         redirect_to [:root], notice: "Your wish has been added!" 
-      else
-         redirect_to [:root], alert: @post.errors.full_messages
-      end
+      @post = Post.create(params[:post])
+      # if @post.save
+      #    redirect_to [:root], notice: "Your wish has been added!" 
+      # else
+      #    redirect_to [:root], alert: @post.errors.full_messages
+      # end
       respond_with @post, location: "/"
    end
 end
